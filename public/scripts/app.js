58dc8c0d5b41aaf0e175bd7125820bfbc2aa652a
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-01-24 12:38:06 +0000

    Use of JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,77 @@
-"use strict";
+'use strict';
 
 console.log("Running");
 
 // JSX - Javascript XML
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Time to spin the wheel'
+
+};
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
+);
+
+var user = {
+    name: 'Bradley',
+    age: 22,
+    location: 'Swansea'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,43 @@
 console.log("Running")
 
 // JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Time to spin the wheel',
+
+}
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
+
+const user = {
+    name: 'Bradley',
+    age: 22,
+    location: 'Swansea'
+}
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>
+    }
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        <p>Age: {user.age}</p>
+        {getLocation(user.location)}
+    </div>
+);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);

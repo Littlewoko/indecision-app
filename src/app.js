96c941921ca94a33410a9a930c056d4d54f52762
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-01-24 15:11:23 +0000

    getting started on forms so removing tester code

--- src/app.js
+++ src/app.js
@@ -14,35 +14,7 @@ const template = (
         {app.subtitle && <p>{app.subtitle}</p>}
         {app.options.length > 0 ? 'Here are your options:' : 'No options available'}
     </div>
-);
+)
 
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCount();
-}
-const minOne = () => {
-    count--
-    renderCount()
-}
-const reset = () => {
-    count = 0
-    renderCount()
-}
-
-const appRoot = document.getElementById('app');
-
-const renderCount = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-
-    ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCount();
+const appRoot = document.getElementById('app')
+ReactDOM.render(template, appRoot)
---
+++ src/playground/counter-example.js
@@ -0,0 +1,30 @@
+let count = 0;
+const addOne = () => {
+    count++;
+    renderCount();
+}
+const minOne = () => {
+    count--
+    renderCount()
+}
+const reset = () => {
+    count = 0
+    renderCount()
+}
+
+
+
+const renderCount = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCount();

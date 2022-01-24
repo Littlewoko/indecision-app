169eadaf07b0c0be6a5838c74e10163aed6c5a48
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-01-24 14:51:48 +0000

    removed user template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,40 +26,6 @@ var template = React.createElement(
     app.options.length > 0 ? 'Here are your options:' : 'No options available'
 );
 
-var user = {
-    name: 'Bradley',
-    age: 22,
-    location: 'Swansea'
-};
-
-var getLocation = function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-};
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -16,25 +16,7 @@ const template = (
     </div>
 );
 
-const user = {
-    name: 'Bradley',
-    age: 22,
-    location: 'Swansea'
-}
 
-const getLocation = (location) => {
-    if (location) {
-        return <p>Location: {location}</p>
-    }
-}
-
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {(user.age && user.age >=18) && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
 
 const appRoot = document.getElementById('app');
 

a561ebca7584ecd18cc064ded7cf27aae0fa1e3c
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-01-24 15:06:12 +0000

    manual data binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,6 +26,50 @@ var template = React.createElement(
     app.options.length > 0 ? 'Here are your options:' : 'No options available'
 );
 
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    renderCount();
+};
+var minOne = function minOne() {
+    count--;
+    renderCount();
+};
+var reset = function reset() {
+    count = 0;
+    renderCount();
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderCount = function renderCount() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'reset'
+        )
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCount();
--- src/app.js
+++ src/app.js
@@ -16,8 +16,33 @@ const template = (
     </div>
 );
 
-
+let count = 0;
+const addOne = () => {
+    count++;
+    renderCount();
+}
+const minOne = () => {
+    count--
+    renderCount()
+}
+const reset = () => {
+    count = 0
+    renderCount()
+}
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+const renderCount = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCount();

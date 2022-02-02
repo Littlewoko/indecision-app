b5f392d02b3770ff05841f890ea5b08f9a5c6cb6
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-02 15:59:18 +0000

    css styling, make everything red

--- src/app.js
+++ src/app.js
@@ -5,5 +5,7 @@ import ReactDOM from 'react-dom'
 // Component Imports
 import IndecisionApp from './components/IndecisionApp'
 
+// CSS style import
+import './styles/styles.css'
 // Final Render
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
